4b511abf4cc576d3816593d475ee9152432a094c
Prakash Divy <prakashdivy@Prakashs-MacBook-Pro.local>
2016-10-03 16:23:08 +0700

    Add IP Penumpang

--- main.go
+++ main.go
@@ -17,7 +17,9 @@ func main() {
 
 func count(c *gin.Context) {
 	banyakCall++
+	var ipsource = c.ClientIP()
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"BanyakCall": banyakCall,
+		"From":       ipsource,
 	})
 }
